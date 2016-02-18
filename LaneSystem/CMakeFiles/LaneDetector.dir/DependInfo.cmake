80d503a015f65e57c58d37fd238a55080f9587f8
tsoutso <s.tsoutsou@tue.nl>
2016-02-18 12:16:21 +0100

    with CMakeLists

---
+++ LaneSystem/CMakeFiles/LaneDetector.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/LaneDetector/DetectLanes.cpp" "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/CMakeFiles/LaneDetector.dir/LaneDetector/DetectLanes.cpp.o"
+  "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/LaneDetector/ExtractFeatures.cpp" "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/CMakeFiles/LaneDetector.dir/LaneDetector/ExtractFeatures.cpp.o"
+  "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/LaneDetector/FittingCurve.cpp" "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/CMakeFiles/LaneDetector.dir/LaneDetector/FittingCurve.cpp.o"
+  "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/LaneDetector/GenerateLaneIndicators.cpp" "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/CMakeFiles/LaneDetector.dir/LaneDetector/GenerateLaneIndicators.cpp.o"
+  "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/LaneDetector/IPMTransformation.cpp" "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/CMakeFiles/LaneDetector.dir/LaneDetector/IPMTransformation.cpp.o"
+  "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/LaneDetector/LaneDetectorTools.cpp" "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/CMakeFiles/LaneDetector.dir/LaneDetector/LaneDetectorTools.cpp.o"
+  "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/LaneDetector/TrackLanes.cpp" "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/CMakeFiles/LaneDetector.dir/LaneDetector/TrackLanes.cpp.o"
+  "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/LaneDetectorSim/Process_LaneDetectorSim.cpp" "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/CMakeFiles/LaneDetector.dir/LaneDetectorSim/Process_LaneDetectorSim.cpp.o"
+  "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/LaneDetectorSim/main_LaneDetectorSim.cpp" "/home/sapfo/Desktop/Lane_git/lanegit/LaneSystem/CMakeFiles/LaneDetector.dir/LaneDetectorSim/main_LaneDetectorSim.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ LaneSystem/CMakeFiles/LaneDetector.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/LaneDetector.dir/LaneDetectorSim/main_LaneDetectorSim.cpp.o"
+  "CMakeFiles/LaneDetector.dir/LaneDetectorSim/Process_LaneDetectorSim.cpp.o"
+  "CMakeFiles/LaneDetector.dir/LaneDetector/LaneDetectorTools.cpp.o"
+  "CMakeFiles/LaneDetector.dir/LaneDetector/ExtractFeatures.cpp.o"
+  "CMakeFiles/LaneDetector.dir/LaneDetector/GenerateLaneIndicators.cpp.o"
+  "CMakeFiles/LaneDetector.dir/LaneDetector/TrackLanes.cpp.o"
+  "CMakeFiles/LaneDetector.dir/LaneDetector/IPMTransformation.cpp.o"
+  "CMakeFiles/LaneDetector.dir/LaneDetector/FittingCurve.cpp.o"
+  "CMakeFiles/LaneDetector.dir/LaneDetector/DetectLanes.cpp.o"
+  "LaneDetector.pdb"
+  "LaneDetector"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/LaneDetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
