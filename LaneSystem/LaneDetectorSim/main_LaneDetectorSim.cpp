28e9f616da91da89b3822ad5f85d70fa7ff5b70e
Seifu <g.n.seifu@tue.nl>
2016-03-01 11:22:51 +0100

    new branch for ROS

--- LaneSystem/LaneDetectorSim/main_LaneDetectorSim.cpp
+++ LaneSystem/LaneDetectorSim/main_LaneDetectorSim.cpp
@@ -1,6 +1,6 @@
 
 #include "main_LaneDetectorSim.h"
-#include "Process_LaneDetectorSim.h"      
+#include "Process_LaneDetectorSim.h"
 #include <stdexcept>
 
 #ifdef __cplusplus
@@ -42,26 +42,23 @@ extern const int    TH_KALMANFILTER     = 1; // originally 1
 
 
 namespace LaneDetectorSim {
-    
+
 	int Process(int argc, const char* argv[])
 	{
-        	if(argc < 9)
+        	if(argc < 5)
             	std::cout << "Not enough parameters" << std::endl;
 
-		int	LANE_DETECTOR 	= atoi(argv[1]);
-		int	LANE_ANALYZER 	= atoi(argv[2]);
-		int	SEND_DATA     	= atoi(argv[3]);
-		int	DATA_RECORD   	= atoi(argv[4]);
-		int	StartFrame	= atoi(argv[5]); // FRAME_START
-		int EndFrame		= atoi(argv[6]); // FRAME_END
-        	double YAW_ANGLE    = atof(argv[7]); // yaw - X
-        	double PITCH_ANGLE  = atof(argv[8]); // pitch - Y
+		int	LANE_DETECTOR  	= atoi(argv[1]);
+	//	int	LANE_ANALYZER 	= atoi(argv[2]);
+	//	int	SEND_DATA     	= atoi(argv[3]);
+	//	int	DATA_RECORD   	= atoi(argv[2]);
+		int	StartFrame    	= atoi(argv[2]); // FRAME_START
+		int EndFrame	    	= atoi(argv[3]); // FRAME_END
+    double YAW_ANGLE    = atof(argv[4]); // yaw - X
+    double PITCH_ANGLE  = atof(argv[5]); // pitch - Y
 
 		std::cout << "/*************************************/" << std::endl;
 		std::cout << "Input LANE_DETECTOR" << LANE_DETECTOR << std::endl;
-		std::cout << "Input LANE_ANALYZER" << LANE_ANALYZER << std::endl;
-		std::cout << "Input SEND_DATA" << SEND_DATA << std::endl;
-		std::cout << "Input DATA_RECROD" << DATA_RECORD << std::endl;
 		std::cout << "Input StartFrame" << StartFrame << std::endl;
 		std::cout << "Input EndFrame" << EndFrame << std::endl;
 		std::cout << "Input YAW_ANGLE" << YAW_ANGLE << std::endl;
@@ -80,20 +77,16 @@ namespace LaneDetectorSim {
 		double lastStartTime    = (double)cv::getTickCount();
 		char key;
 		double delay = 1;
-		
+
 		std::ofstream laneFeatureFile;
-		if (DATA_RECORD)
-		{
-		    	InitRecordData(laneFeatureFile, FILE_LANE_FEATURE, laneFeatureName, NUM_LANE);
-		}
-        
+
 		/* Parameters for Lane Detector */
 		cv::Mat laneMat;
-		LaneDetector::LaneDetectorConf laneDetectorConf; 
+		LaneDetector::LaneDetectorConf laneDetectorConf;
 		std::vector<cv::Vec2f> hfLanes;
 		std::vector<cv::Vec2f> lastHfLanes;
 		std::vector<cv::Vec2f> preHfLanes;
-		
+
 		std::vector<double> LATSDBaselineVec;
 		std::deque<LaneDetector::InfoCar> lateralOffsetDeque;
 		std::deque<LaneDetector::InfoCar> LANEXDeque;
@@ -116,189 +109,73 @@ namespace LaneDetectorSim {
 		InitlaneFeatures(laneFeatures);
 		GetSamplingTime(LANE_RECORD_FILE, samplingTime);
 
-        	if (LANE_DETECTOR) 
-		{ 
-             		/* Lane detect and tracking */ 
+        	if (LANE_DETECTOR)
+		{
+             		/* Lane detect and tracking */
             		sprintf(laneImg, LANE_RAW_NAME , idx);
             		laneMat = cv::imread(laneImg);
             		LaneDetector::InitlaneDetectorConf(laneMat, laneDetectorConf, 2); // KIT 1, ESIEE 2
             		LaneDetector::InitLaneKalmanFilter(laneKalmanFilter, laneKalmanMeasureMat, laneKalmanIdx);
         	}
-        
+
         	/* Inter-process communication */
         	key_t ipckey;
         	int mq_id;
-        	struct 
-		{ 
-            		long type; 
-            		char text[1024]; 
+        	struct
+	      	{
+            		long type;
+            		char text[1024];
         	} laneMsg;
 
-        	if (SEND_DATA) 
-        	{
-            		/* Generate the ipc key */
-            		ipckey = ftok(KEY_PATH, 'a');
-            		if(ipckey == -1)
-			{
-                	printf("Key Error: %s\n", strerror(errno));
-                	exit(1);
-            		}
-            
-		    	mq_id = msgget(ipckey, 0);
-		    	if (mq_id == -1) 
-			{ 
-		        	//MQ doesn't exit
-		        	mq_id = msgget(ipckey, IPC_CREAT | IPC_EXCL | 0666);
-		        	printf("LaneDetector creates a new MQ %d\n", mq_id);
-		    	} else {
-		        	//MQ does exit
-		        	mq_id = msgget(ipckey, IPC_EXCL | 0666);
-		        	printf("LaneDetector uses an existed MQ %d\n", mq_id);
-		    	}
-		    	//printf("Lane identifier is %d\n", mq_id);
-		    	if(mq_id == -1) 
-			{  
-		        	throw std::logic_error("Can't build pipeline"); 
-		    	}
-		    	//printf("This is the LaneDetectSim process, %d\n", getpid());
-        	}
-        
 		double delayTime;
 		if(idx == 1)
 		delayTime = 0;
 		else
 		delayTime = samplingTime.at(idx - 2);
-        
+
         	/* Entrance of Process */
         	while (idx <= EndFrame)
         	{
             		double startTime = (double)cv::getTickCount();
 
-            		/* Lane detect and tracking */ 
+            		/* Lane detect and tracking */
             		sprintf(laneImg, LANE_RAW_NAME , idx);
             		laneMat = cv::imread(laneImg);//imshow("laneMat", laneMat);
 
             		if (LANE_DETECTOR)
             		{
-                		ProcessLaneImage(laneMat, laneDetectorConf, startTime, laneKalmanFilter, laneKalmanMeasureMat, laneKalmanIdx, hfLanes, lastHfLanes, 
-				lastLateralOffset, lateralOffset, isChangeLane, detectLaneFlag,  idx, execTime, preHfLanes, changeDone, YAW_ANGLE, PITCH_ANGLE);
+                		ProcessLaneImage(laneMat, laneDetectorConf, startTime, laneKalmanFilter, laneKalmanMeasureMat, laneKalmanIdx, hfLanes, lastHfLanes,
+			            	lastLateralOffset, lateralOffset, isChangeLane, detectLaneFlag,  idx, execTime, preHfLanes, changeDone, YAW_ANGLE, PITCH_ANGLE);
             		}
-            
+
             		intervalTime = (startTime - lastStartTime)/ cv::getTickFrequency();//get the time between two continuous frames
             		lastStartTime = startTime;
             		//std::cout << "intervalTime: "<< intervalTime << std::endl;
-            
-            		/* Generate lane indicators */
-            		if (LANE_DETECTOR && LANE_ANALYZER)
-            		{
-                		// First init the baseline, then get lane mass 
-                		if( pastTime < TIME_BASELINE + delayTime)
-				{
-                    			// Get lane baseline
-                   			LaneDetector::GetLaneBaseline(sampleIdx, SAMPLING_TIME, muWindowSize, sigmaWindowSize, lateralOffset, LATSDBaselineVec,
-                                    	lateralOffsetDeque, LANEXDeque,	TLCDeque, laneFeatures, intervalTime);
-                    			//idx_baseline = sampleIdx;
-                		} else {
-                    			//sampleIdx -= idx_baseline;
-                    			LaneDetector::GenerateLaneIndicators(sampleIdx, SAMPLING_TIME, muWindowSize, sigmaWindowSize, lateralOffset,                                  						lateralOffsetDeque, LANEXDeque, TLCDeque, laneFeatures, intervalTime);
-		            		//! LATSD
-		            		char *text_LATSD = new char[30];
-		            		sprintf(text_LATSD, "L1. LATSD: %.4f", laneFeatures.LATSD);
-		            		cv::putText(laneMat, text_LATSD, cv::Point(0, 70), cv::FONT_HERSHEY_SIMPLEX, 0.3, CV_RGB(0,255,0));
-		            		delete text_LATSD;
-		            		//! LATMEAN
-		            		char *text_LATMEAN = new char[30];
-		            		sprintf(text_LATMEAN, "L2. LATMEAN: %.4f", laneFeatures.LATMEAN);
-		            		cv::putText(laneMat, text_LATMEAN, cv::Point(0, 80), cv::FONT_HERSHEY_SIMPLEX, 0.3, CV_RGB(0,255,0));
-		            		delete text_LATMEAN;
-		            		//! LANEDEV
-		            		char *text_LANEDEV = new char[30];
-		            		sprintf(text_LANEDEV, "L3. LANEDEV: %.4f", laneFeatures.LANEDEV);
-		            		cv::putText(laneMat, text_LANEDEV, cv::Point(0, 90), cv::FONT_HERSHEY_SIMPLEX, 0.3, CV_RGB(0,255,0));
-		            		delete text_LANEDEV;
-		            		//! LANEX
-		            		char *text_LANEX = new char[30];
-		            		sprintf(text_LANEX, "L4. LANEX: %.4f", laneFeatures.LANEX);
-		            		cv::putText(laneMat, text_LANEX, cv::Point(0, 100), cv::FONT_HERSHEY_SIMPLEX, 0.3, CV_RGB(0,255,0));
-		            		delete text_LANEX;
-		            		//! TLC
-		           		char *text_TLC = new char[30];
-		            		sprintf(text_TLC, "L5. TLC: %.4f", laneFeatures.TLC);
-		            		cv::putText(laneMat, text_TLC, cv::Point(0, 110), cv::FONT_HERSHEY_SIMPLEX, 0.3, CV_RGB(0,255,0));
-		            		delete text_TLC;
-		            		//! TLC_2s
-		            		char *text_TLC_2s = new char[30];
-		            		sprintf(text_TLC_2s, "L6. TLC_2s: %d", laneFeatures.TLC_2s);
-		            		cv::putText(laneMat, text_TLC_2s, cv::Point(0, 120), cv::FONT_HERSHEY_SIMPLEX, 0.3, CV_RGB(0,255,0));
-		            		delete text_TLC_2s;
-		            		char *text_TLCF_2s = new char[50];
-		            		sprintf(text_TLCF_2s, "L7. Fraction_TLC_2s: %f", laneFeatures.TLCF_2s);
-		            		cv::putText(laneMat, text_TLCF_2s, cv::Point(0, 130), cv::FONT_HERSHEY_SIMPLEX, 0.3, CV_RGB(0,255,0));
-		            		delete text_TLCF_2s;
-		            		//! TLC_halfs
-		            		char *text_TLC_halfs = new char[30];
-		            		sprintf(text_TLC_halfs, "L8. TLC_halfs: %d", laneFeatures.TLC_halfs);
-		            		cv::putText(laneMat, text_TLC_halfs, cv::Point(0, 140), cv::FONT_HERSHEY_SIMPLEX, 0.3, CV_RGB(0,255,0));
-		            		delete text_TLC_halfs;
-		         		char *text_TLCF_halfs = new char[50];
-		            		sprintf(text_TLCF_halfs, "L9. Fraction_TLC_halfs: %f", laneFeatures.TLCF_halfs);
-		            		cv::putText(laneMat, text_TLCF_halfs, cv::Point(0, 150), cv::FONT_HERSHEY_SIMPLEX, 0.3, CV_RGB(0,255,0));
-		            		delete text_TLCF_halfs;
-		            		//! TLC_min
-		            		char *text_TLC_min = new char[30];
-		            		sprintf(text_TLC_min, "L10. TLC_min: %.4f", laneFeatures.TLC_min);
-		            		cv::putText(laneMat, text_TLC_min, cv::Point(0, 160), cv::FONT_HERSHEY_SIMPLEX, 0.3, CV_RGB(0,255,0));
-		            		delete text_TLC_min;
-                		}
-            		}//end Generate Lane Indicators
-            
+
+
             		if(IMAGE_RECORD)
-			{
+	         		{
                 		char *text = new char[100];
                 		sprintf(text, LANE_RECORD_IMAGE, idx);
                 		cv::imwrite(text, laneMat);
                 		delete text;
             		}
 
-            		/* Send the datas as string to fusion center */
-            		if(LANE_DETECTOR & SEND_DATA) 
-			{
-				char *str = new char[1024];
-				memset(str, 0, 1024);
-				CodeMsg(laneFeatures, str);
-				
-				strcpy(laneMsg.text, str);//!!! overflow 
-				laneMsg.type = 1;
-				//printf("Data sends: %s\n", laneMsg.text);
-				
-				//! 0 will cause a block/ IPC_NOWAIT will close the app.
-				if(msgsnd(mq_id, &laneMsg, sizeof(laneMsg), 0) == -1)
-				{  
-				    	throw std::runtime_error("LaneDetectSim: msgsnd failed!");  
-				}  
-				delete str;
-            		}
-            
+
             		/* Adjust the interval time within fixed frequency */
-			double execFreq; 
-            		do 
-			{
+			double execFreq;
+            		do
+		              {
                 		execFreq = 1.0 / (((double)cv::getTickCount() - startTime)/cv::getTickFrequency());
                 		pastTime = ((double)cv::getTickCount() - initTime)/cv::getTickFrequency() + delayTime;
            		//	} while ( pastTime < samplingTime.at(idx-1) );
              			} while(execFreq > SAMPLING_FREQ);
              		//	} while(pastTime < (double)sampleIdx/SAMPLING_FREQ);
 
-			/* Record lane features */
-            		if (DATA_RECORD) 
-			{
-                		RecordLaneFeatures(laneFeatureFile, laneFeatures, execTime, pastTime);
-            		}
-
             		char *text = new char[30];
             		sprintf(text, "past time: %.2f sec", pastTime);
             		cv::putText(laneMat, text, cv::Point(0, laneMat.rows-5), cv::FONT_HERSHEY_SIMPLEX, 0.8, CV_RGB(0,255,0));
-            
+
             		sprintf(text, "Adjusted Freq: %.2f Hz", execFreq);
             		cv::putText(laneMat, text, cv::Point(0, 60), cv::FONT_HERSHEY_SIMPLEX, 0.8, CV_RGB(0, 255, 0));
             		delete text;
@@ -321,11 +198,11 @@ namespace LaneDetectorSim {
             		// 	idx = FRAME_RESTART1;
             		// if(idx == FRAME_STOP2)
             		//	idx = FRAME_RESTART2;
-        	}//end while loop 
+        	}//end while loop
 
         	laneFeatureFile.close();
         	cv::destroyAllWindows();
-        
+
         	return 0;
     	}
 }//FusedCarSurveillanceSim
@@ -335,5 +212,6 @@ namespace LaneDetectorSim {
 using LaneDetectorSim::Process;
 int main(int argc, const char * argv[])
 {
+
     	return Process(argc, argv);
 }
